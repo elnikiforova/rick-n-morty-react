9454c2f546b9ab042de25d5199c85453912b9239
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-22 15:53:11 +0300

    added subscribe

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,12 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-export const store = createStore(reducer);
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {}
+
+export const store = createStore(reducer, persistedState);
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
